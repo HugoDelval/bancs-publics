7751f74502cfb1d0559b694ff67f1b61aa8c4b65
Hugo Delval <hugo.delval@gmail.com>
2022-02-03 12:06:04 +0100

    nouveaux parrainages

--- website/parrainages/index.php
+++ website/parrainages/index.php
@@ -17,97 +17,99 @@
 
       <h2 class="title is-4">Liste complète par ordre alphabétique :</h2>
       <ul>
-        <li>Françoise Almartine, écologiste</li>
-        <li>Alban et Mathilde, usagers du parc pour enfants</li>
+        <li>Françoise Alamartine, écologiste </li>
+        <li>Alban et Mathilde, usagers du parc pour enfants </li>
         <li>Yann Arthus-Bertrand, photographe</li>
-        <li>Association des riverains de la place Aristide Briand</li>
-        <li>Maryse Auzat, retraitée</li>
+        <li>Association des riverains de la place Aristide Briand</li>
+        <li>Maryse Auzat, retraitée </li>
         <li>Diane B., Sétoise écolo</li>
-        <li>Guilhem Birouste, médecin généraliste</li>
-        <li>Luisa Calcine, gynécologue</li>
-        <li>Cécile Bois, actrice</li>
+        <li>Guilhem Birouste, médecin généraliste </li>
+        <li>Luisa Calcine, gynécologue </li>
+        <li>Cécile Bois, actrice </li>
         <li>Thomas Brail, fondateur du GNSA</li>
-        <li>Emilio Cerda, menuisier</li>
+        <li>Emilio Cerda, menuisier </li>
+        <li>Alain Charrié, musicien</li>
         <li>Gaspar Claus, violoncelliste</li>
-        <li>Claude Combas, organisateur d’évènements sétois</li>
-        <li>Gaël Dadies, photographe</li>
-        <li>Danielle, retraitée</li>
-        <li>Demi Portion, rappeur sétois</li>
+        <li>Claude Combas, commerçant</li>
+        <li>Gaël Dadies, photographe </li>
+        <li>Danielle, retraitée </li>
+        <li>Demi Portion, rappeur sétois </li>
         <li>Daniel Dezeuze, artiste plasticien</li>
         <li>Dominique, sétoise et sociologue</li>
         <li>Françoise Doré, décoratrice</li>
-        <li>Gabriel Du Roure, retraité sétois</li>
-        <li>Laure Dugaret, directrice d’école maternelle</li>
-        <li>Edith et Christine, riveraines</li>
+        <li>Charlotte Douzal, écrivaine </li>
+        <li>Gabriel Du Roure, retraité sétois </li>
+        <li>Laure Dugaret, directrice d’école maternelle </li>
+        <li>Edith et Christine, riveraines </li>
         <li>Elsa, assistante sociale</li>
-        <li>Martine Escassut, pédopsychiatre</li>
-        <li>Esma, amoureuse des arbres</li>
-        <li>Éva, pour l’amour de Sète</li>
+        <li>Martine Escassut, pédopsychiatre </li>
+        <li>Éva, pour l’amour de Sète </li>
         <li>Pierre Ferrand, retraité</li>
-        <li>Christian et Martine Ferrari, auteur-compositeur et enseignante, riverains</li>
-        <li>Jean-François Forès, conchyliculteur</li>
+        <li>Christian et Martine Ferrari, auteur-compositeur et enseignante, riverains </li>
+        <li>Jean-François Forès, conchyliculteur </li>
         <li>Gabriel, élève de cm2</li>
+        <li>Sophie Garayoa, libraire</li>
         <li>Cyrielle Garrigues, amoureuse de la nature</li>
         <li>Patricia Gomé</li>
-        <li>Jean Graybeal, retraitée et riveraine</li>
-        <li>Christian Hernandez, président d’association</li>
+        <li>Jean Graybeal, retraitée et riveraine </li>
+        <li>Christian Hernandez, président d’association </li>
         <li>Bernard Heymann, intermittent</li>
         <li>Hippolyte, élève de CM1</li>
-        <li>Max Horde, artiste</li>
-        <li>Jade, élève de maternelle</li>
-        <li>Emmanuelle Jans, sétoise</li>
+        <li>Max Horde, artiste </li>
+        <li>Imbert Imbert, chanteur-contrebassiste</li>
+        <li>Jade, élève de maternelle </li>
+        <li>Emmanuelle Jans, sétoise </li>
         <li>Arnaud Jean, secrétaire général CGT de Sète et jouteur</li>
-        <li>François Jean-Jacques, artiste peintre sétois</li>
-        <li>Jocelyne et François, riverains</li>
-        <li>Damien Jouve, enseignant</li>
+        <li>François Jean-Jacques, artiste peintre sétois </li>
+        <li>Jocelyne et François, riverains </li>
+        <li>Damien Jouve, enseignant </li>
         <li>François Julliot, chorégraphe</li>
-        <li>Marie-Thérèse Kassianides, retraitée</li>
-        <li>Kayss, élève de cm2</li>
+        <li>Marie-Thérèse Kassianides, retraitée </li>
         <li>Lalala Ukulélé Club</li>
-        <li>Famille Lalia, en résistance</li>
+        <li>Famille Lalia, en résistance </li>
+        <li>Pascal Larderet, Cie Cacahète </li>
         <li>Laury, Ysé et Pia, famille en vacance</li>
-        <li>Denis Lavant, acteur</li>
-        <li>Martine Leclere, infirmière</li>
-        <li>Philippe Leclere, retraité et riverain</li>
-        <li>Lila, élève de maternelle</li>
-        <li>François Liberti, ancien député-maire de la ville de Sète</li>
+        <li>Denis Lavant, acteur </li>
+        <li>Martine Leclere, infirmière </li>
+        <li>Philippe Leclere, retraité et riverain </li>
+        <li>Lila, élève de maternelle </li>
+        <li>François Liberti, ancien député-maire de la ville de Sète </li>
         <li>Lionel Lopez, chanteur du groupe Les Mourres de porc</li>
         <li>Florence Loste, analyste</li>
         <li>Lucie, élève de CE2</li>
-        <li>Jacques Maillet, retraité</li>
-        <li>Yves Marchand, ancien maire de Sète</li>
+        <li>Jacques Maillet, retraité </li>
+        <li>Yves Marchand, ancien maire de Sète </li>
         <li>Marie et Didier, anciens commerçants</li>
-        <li>Marie et Djemila, prof de yoga et prof d’arts-plastiques</li>
-        <li>Esther Marlot, peintre et musicienne</li>
-        <li>Mathilde et Malone, famille de riverains</li>
-        <li>Maxence, élève de CP</li>
+        <li>Marie et Djemila, prof de yoga et prof d’arts-plastiques </li>
+        <li>Esther Marlot, peintre et musicienne </li>
+        <li>Mathilde et Malone, famille de riverains </li>
+        <li>Maxence, élève de CP </li>
         <li>Valérie Mazat-Baum, commerçante</li>
-        <li>Guillaume Meurice, humoriste</li>
-        <li>Pierre Mistral, retraité riverain</li>
-        <li>Mohamed et Bilal, écoliers</li>
-        <li>Jérôme Muron, éducateur</li>
-        <li>Nathalie et Michèle, Sétoises et riveraines</li>
-        <li>Nedji et Raphaël, usagers du parc pour enfants</li>
-        <li>Nicole, GNSA du bitterois</li>
-        <li>Michèle Parcé, ancienne conseillère municipale</li>
-        <li>Petitcopek, chanteur de rap sétois</li>
+        <li>Guillaume Meurice, humoriste </li>
+        <li>Pierre Mistral, retraité riverain </li>
+        <li>Jérôme Muron, éducateur </li>
+        <li>Nathalie et Michèle, Sétoises et riveraines </li>
+        <li>Nedji et Raphaël, usagers du parc pour enfants </li>
+        <li>Nicole, GNSA du bitterois </li>
+        <li>Michèle Parcé, ancienne conseillère municipale </li>
+        <li>Petitcopek, chanteur de rap sétois </li>
         <li>Daniel Rigaud, La roue libre de Thau</li>
-        <li>Lucette Roig, ancienne élue et militante associative</li>
+        <li>Lucette Roig, ancienne élue et militante associative </li>
         <li>Sacha, élève de CP</li>
-        <li>Jean-Claude Sauvagnargues, ancien directeur de l’Ifremer Sète</li>
+        <li>Jean-Claude Sauvagnargues, ancien directeur de l’Ifremer Sète </li>
         <li>Sisobr, ingénieur logiciel</li>
         <li>Didier Super, chanteur</li>
-        <li>Susan Sajno, retraitée</li>
-        <li>Lydie Salvayre, écrivaine</li>
+        <li>Susan Sajno, retraitée </li>
+        <li>Lydie Salvayre, écrivaine </li>
         <li>Pedro Soler, guitariste</li>
         <li>Bruno Solo, acteur</li>
         <li>Kakine Rougier, retraitée</li>
-        <li>Daphné Rebouillat, créatrice de bijoux</li>
+        <li>Daphné Rebouillat, créatrice de bijoux </li>
         <li>Benjamin Trichat, danseur et chorégraphe</li>
-        <li>Véronique et Cécile, Sétoises</li>
+        <li>Véronique et Cécile, Sétoises </li>
         <li>Violaine Vérité, comédienne à Sète</li>
         <li>Florent Viguié, boulanger</li>
-        <li>Daniel Villanova, humoriste et acteur</li>
+        <li>Daniel Villanova, humoriste et acteur </li>
         <li>Alain Zarouati, plasticien sétois </li>
       </ul>
     </div>
